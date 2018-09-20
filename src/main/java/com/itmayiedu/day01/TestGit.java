14fc69b23ee1ee6d65e1820e301cfbccdc6758a3
yantianpeng <hkdyantianpeng@163.com>
2018-09-20 14:40:11 +0800

    hello

--- src/main/java/com/itmayiedu/day01/TestGit.java
+++ src/main/java/com/itmayiedu/day01/TestGit.java
@@ -4,4 +4,11 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试git数据的");
     }
+
+    public static boolean isEmpty(String string){
+        if(string.length()==0||"".equals(string)||null==string){
+            return  true;
+        }
+        return false;
+    }
 }
