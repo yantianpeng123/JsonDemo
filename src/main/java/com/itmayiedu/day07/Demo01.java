cb5996b2ee5430894538b564ff278f5244193baa
yantianpeng <hkdyantianpeng@163.com>
2018-10-13 14:33:27 +0800

    删除测试文件

--- src/main/java/com/itmayiedu/day07/Demo01.java
+++
@@ -1,7 +0,0 @@
-package com.itmayiedu.day07;
-
-public class Demo01 {
-    public static String getStr(){
-        return "测试分之代码";
-    }
-}
--- src/main/java/com/itmayiedu/day07/Demo02.java
+++
@@ -1,8 +0,0 @@
-package com.itmayiedu.day07;
-
-public class Demo02 {
-    public static String getStr(){
-        return "day08";
-    }
-}
-
